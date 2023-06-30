73940751f521ce61af23f54d94c41226adc77772
aryansahoo0912 <jeetan.bdk@gmail.com>
2023-07-01 01:31:25 +0530

    commit

--- coursera.js
+++ coursera.js
@@ -1,10 +1,23 @@
 var name="Aryan"
 var greet="Hello"
 console.log(greet,name)
-var result=9;
+
+
+var result=98;
 if(result>90){
     console.log("Passed")
 }
 else{
     console.log("Failed")
+}
+
+var place='second'
+if(place=='first'){
+    console.log('Gold')
+}else if(place='second'){
+    console.log('Silver')
+}else if (place='third'){
+    console.log('Bronze')
+}else{
+    console.log('No Medal')
 }
